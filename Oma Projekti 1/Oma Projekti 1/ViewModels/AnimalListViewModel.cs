d35993cd9d142a3dc762418fcb4b2f0fc169ffc7
Sami Kallio <sami.kallio@lci1.com>
2022-01-27 14:45:06 -0500

    Added Animal List Page

---
+++ Oma Projekti 1/Oma Projekti 1/ViewModels/AnimalListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using Oma_Projekti_1.Services;
+
+namespace Oma_Projekti_1.ViewModels
+{
+    public class AnimalListViewModel : BaseViewModel
+    {
+        private ObservableCollection<IAnimal> _animals;
+
+        public AnimalListViewModel()
+        {
+            _animals = new ObservableCollection<IAnimal>(
+                new IAnimal[]
+                {
+                    new Cat(),
+                    new Dog()
+                });
+        }
+
+        public ObservableCollection<IAnimal> Animals => _animals;
+    }
+}
--- Oma Projekti 1/Oma Projekti 1/Views/AnimalListPage.xaml.cs
+++ Oma Projekti 1/Oma Projekti 1/Views/AnimalListPage.xaml.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Oma_Projekti_1.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Oma_Projekti_1.ViewModels
+namespace Oma_Projekti_1.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AnimalListView : ContentPage
+    public partial class AnimalListPage : ContentPage
     {
-        public AnimalListView()
+        public AnimalListPage()
         {
             InitializeComponent();
+
+            BindingContext = new AnimalListViewModel();
         }
-
-
-
-
-        public string Animal { get; set; }
-
     }
-
 }
