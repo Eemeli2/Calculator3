d3ea272d414dc11aa3533a415ce9ef345d63303d
Eemeli2 <metso.eemeli@gmail.com>
2022-02-22 18:26:48 +0200

    Starges commited

--- BulletSolution/Services/Reloading.Services.EF/EntityBulletService.cs
+++ BulletSolution/Services/Reloading.Services.EF/EntityBulletService.cs
@@ -34,12 +34,22 @@ namespace Reloading.Services.EF
         {
             using (var db = new ReloadingContext())
             {
+                //if (bullet.Diameter!.Id.HasValue) bullet.Diameter = await db.Diameters.FirstAsync(d => d.Id!.Equals(bullet.Diameter.Id!));
+                //if (bullet.Mass!.Id.HasValue) bullet.Mass = await db.Masses.FirstAsync(d => d.Id!.Equals(bullet.Mass.Id!));
+                //if (bullet.Manufacturer!.Id.HasValue) bullet.Manufacturer = await db.Manufacturers.FirstAsync(d => d.Id!.Equals(bullet.Manufacturer.Id!));
+
+                if (bullet.Diameter!.Id.HasValue) db.Diameters.Attach(bullet.Diameter!);
+                if (bullet.Mass!.Id.HasValue) db.Masses.Attach(bullet.Mass!);
+                if (bullet.Manufacturer!.Id.HasValue) db.Manufacturers.Attach(bullet.Manufacturer!);
+
+                //db.Manufacturers.Attach(bullet.Manufacturer!);
+                //db.Diameters.Attach(bullet.Diameter!);
+                //db.Masses.Attach(bullet.Mass!);
+
                 var b = await db.Bullets.AddAsync(bullet);
                 await db.SaveChangesAsync();
                 return bullet.Id.Value;
-
             }
-
         }
     }
 }
