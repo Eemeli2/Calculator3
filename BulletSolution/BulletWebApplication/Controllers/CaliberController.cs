86abbcb12c005f278251b943c75a17a60de9dc8d
Eemeli2 <metso.eemeli@gmail.com>
2022-03-15 20:01:44 +0200

    Stages commited

---
+++ BulletSolution/BulletWebApplication/Controllers/CaliberController.cs
@@ -0,0 +1,42 @@
+﻿using BulletLibrary;
+using BulletLibrary.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulletWebApplication.Controllers
+{
+    [Route("api/caliber")]
+    [ApiController]
+    public class CaliberController : ControllerBase, ICaliberService
+    { 
+      private readonly ILogger<ReloadController> _logger;
+      private readonly ICaliberService _caliberService;
+        
+        public CaliberController(ILogger<ReloadController> logger, ICaliberService caliberService)
+        {
+        _logger = logger;
+        _caliberService = caliberService;
+        }
+        
+        [HttpGet]
+        [Route("GetAll")]
+        public Task<IEnumerable<Caliber>> GetAll()
+        {
+           return _caliberService.GetAll();
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public Task<IEnumerable<Caliber>> GetAllByDiameter(Diameter diameter)
+        {
+            return _caliberService.GetAllByDiameter(diameter);
+        }
+
+        [HttpGet]
+        [Route("Insert")]
+        public Task<Guid> Insert([FromBody]Caliber caliber)
+        {
+            return _caliberService.Insert(caliber);
+        }
+    }
+}
---
+++ BulletSolution/BulletWebApplication/Controllers/MeasurementController.cs
@@ -0,0 +1,50 @@
+﻿using BulletLibrary;
+using BulletLibrary.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulletWebApplication.Controllers
+{
+    [Route("api/diameter")]
+    [ApiController]
+
+    public class MeasurementController : ControllerBase, IMeasurementService
+    {
+        private readonly ILogger<ReloadController> _logger;
+        private readonly IMeasurementService _measurementService;
+
+        public MeasurementController(ILogger<ReloadController> logger, IMeasurementService measurementService)
+        {
+            _logger = logger;
+            _measurementService = measurementService;
+        }
+
+        [HttpGet]
+        [Route("GetAllDiameters")]
+        public Task<IEnumerable<Diameter>> GetAllDiameters()
+        {
+            return  _measurementService.GetAllDiameters();
+        }
+
+        [HttpGet]
+        [Route("GetAllMasses")]
+        public Task<IEnumerable<Mass>> GetAllMasses()
+        {
+            return _measurementService.GetAllMasses();
+        }
+
+        [HttpGet]
+        [Route("InsertDiameter")]
+        public Task<Guid> InsertDiameter([FromBody]Diameter diameter)
+        {
+            return _measurementService.InsertDiameter(diameter);
+        }
+
+        [HttpGet]
+        [Route("InsertMass")]
+        public Task<Guid> InsertMass([FromBody]Mass mass)
+        {
+            return _measurementService.InsertMass(mass);
+        }
+    }
+}
